57f942701a8d8582806af9a9ade245841c9ae67a
Caroline Zaini <zainicaroline@gmail.com>
2020-05-13 11:43:57 +0200

    add project

---
+++ ImagePicker/ImagePicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImagePicker
+//
+//  Created by Caroline Zaini on 13/05/2020.
+//  Copyright © 2020 Caroline Zaini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
